0ffe90d23679d9d2bdb57d42047d22dc6281ca76
kathya2006 <nl2069@srmist.edu.in>
2026-04-08 22:51:08 +0530

    UC4 Implemented

--- src/Main.java
+++ src/Main.java
@@ -1,42 +1,55 @@
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
-
-        System.out.println("Room Inventory Listing");
 
         RoomInventory r1 = new RoomInventory("Single Room", 1, 1000.0, 5);
         RoomInventory r2 = new RoomInventory("Double Room", 2, 2000.0, 3);
         RoomInventory r3 = new RoomInventory("Suite Room", 4, 5000.0, 1);
 
-        System.out.println();
-        r1.printRoom();
-        System.out.println();
+        RoomInventoryList list = new RoomInventoryList();
+        list.addRoom(r1);
+        list.addRoom(r2);
+        list.addRoom(r3);
 
-        r2.printRoom();
-        System.out.println();
-
-        r3.printRoom();
+        list.printRooms();
     }
 }
 
 class RoomInventory {
 
-    private String name;
-    private int max;
-    private double pricePerNight;
-    private int numberOfRooms;
+    String name;
+    int max;
+    double pricePerNight;
+    int available;
 
-    public RoomInventory(String name, int max, double pricePerNight, int numberOfRooms) {
+    public RoomInventory(String name, int max, double pricePerNight, int available) {
         this.name = name;
         this.max = max;
         this.pricePerNight = pricePerNight;
-        this.numberOfRooms = numberOfRooms;
+        this.available = available;
     }
 
     public void printRoom() {
         System.out.println(name);
         System.out.println("Max: " + max);
         System.out.println("Price per night: " + pricePerNight);
-        System.out.println("Number of Rooms: " + numberOfRooms);
+        System.out.println("Available: " + available);
+    }
+}
+
+class RoomInventoryList {
+
+    RoomInventory[] rooms = new RoomInventory[10];
+    int count = 0;
+
+    public void addRoom(RoomInventory room) {
+        rooms[count++] = room;
+    }
+
+    public void printRooms() {
+        for (int i = 0; i < count; i++) {
+            rooms[i].printRoom();
+            System.out.println();
+        }
     }
 }
